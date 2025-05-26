238ad9ce99a6850aca8818f8de4a344052e23423
akila <akilsachitra@gmail.com>
2025-05-26 23:11:38 +1200

    Add initial Terraform configuration and variable definitions

---
+++ .github/setup/main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.23.0"
+        }
+    }
+    backend "s3" {
+        bucket         = "devops-akila1"
+        key            = "tf-state-setup"
+        region         = "ap-southeast-2"
+        encrypt        = true
+        dynamodb_table = "devops_terraformlock"    
+      
+    }
+    required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+  default_tags {
+    tags = {
+      Environment = "terraform.workspace"
+      Project     = "var.project"
+      Conatact  = "VAR.contact"
+      MAnagedBy  = "Terraform/setup"    
+    }
+  }
+  
+}
---
+++ .github/setup/variable.tf
@@ -0,0 +1,20 @@
+variable "tf_state_bucket" {
+  description = "S3 bucket for Terraform state"
+  default     = "devops-akila1"
+}
+
+variable "tf_state_lock_table" {
+  description = "DynamoDB table for Terraform state locking"
+  default     = "devops_terraformlock" 
+}
+
+variable "project" {
+  description = "Project name for tagging"
+  default     = "devops-app"
+}
+
+variable "contact" {
+  description = "Contact information for the project"
+  default     = "test@gmail.com"
+ 
+}
